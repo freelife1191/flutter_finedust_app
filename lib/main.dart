df25219f1b4b33fddcd3b1d2164b5ff11aeaffc2
freelife1191 <freelife1191.good@gmail.com>
2019-06-03 00:37:25 +0900

    실제 미세먼지 데이터 표시하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'models/AirResult.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 /**
  * 미세먼지 앱 메인화면
  */
@@ -25,91 +29,152 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
+  AirResult _result;
+
+  //비동기로 데이터 받기
+  Future<AirResult> fetchData() async {
+    var response = await http
+        .get('https://api.airvisual.com/v2/nearest_city?key=wCD5bHTNsPWH2C5pp');
+
+    AirResult result = AirResult.fromJson(json.decode(response.body));
+
+    return result;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetchData().then((airResult) {
+      setState(() {
+        _result = airResult;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                '현재 위치 미세먼지',
-                style: TextStyle(fontSize: 30),
-              ),
-              SizedBox(
-                //여백 주기
-                height: 16,
-              ),
-              Card(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        //글 간격을 공간을 두고 띄움
-                        children: <Widget>[
-                          Text('얼굴사진'),
-                          Text(
-                            '80',
-                            style: TextStyle(fontSize: 40),
-                          ),
-                          Text(
-                            '보통',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ],
+      body: Center(
+        child: _result == null
+            ? CircularProgressIndicator()
+            : Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '현재 위치 미세먼지',
+                        style: TextStyle(fontSize: 30),
                       ),
-                      color: Colors.yellow,
-                      padding: const EdgeInsets.all(8.0),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        //글 간격을 공간을 두고 띄움
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Text('이미지'),
-                              SizedBox(
-                                width: 16,
+                      SizedBox(
+                        //여백 주기
+                        height: 16,
+                      ),
+                      Card(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                //글 간격을 공간을 두고 띄움
+                                children: <Widget>[
+                                  Image.network(
+                                      'http://file.instiz.net/data/file/20120828/2/2/5/225d696edd27b7044443ee8b7af6eba8',
+                                      width: 40),
+                                  Text(
+                                    '${_result.data.current.pollution.aqius}',
+                                    style: TextStyle(fontSize: 40),
+                                  ),
+                                  Text(
+                                    getString(_result),
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ],
                               ),
-                              Text(
-                                '11°',
-                                style: TextStyle(fontSize: 16),
-                              )
-                            ],
-                          ),
-                          Text('습도 100%'),
-                          Text('풍속 5.7m/s'),
-                        ],
+                              color: getColor(_result),
+                              padding: const EdgeInsets.all(8.0),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                //글 간격을 공간을 두고 띄움
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Image.network(
+                                        'https://airvisual.com/images/${_result.data.current.weather.ic}.png',
+                                        width: 32,
+                                        height: 32,
+                                      ),
+                                      SizedBox(
+                                        width: 16,
+                                      ),
+                                      Text(
+                                        '${_result.data.current.weather.tp}°',
+                                        style: TextStyle(fontSize: 16),
+                                      )
+                                    ],
+                                  ),
+                                  Text(
+                                      '습도 ${_result.data.current.weather.hu}%'),
+                                  Text(
+                                      '풍속 ${_result.data.current.weather.ws}m/s'),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    )
-                  ],
+                      SizedBox(
+                        //여백 주기
+                        height: 16,
+                      ),
+                      ClipRRect(
+                        //모서리를 둥글게 해주는 위젯
+                        borderRadius: BorderRadius.circular(30),
+                        child: RaisedButton(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 15.0, horizontal: 50), //버튼에 여백 주기
+                          color: Colors.orange,
+                          child: Icon(Icons.refresh, color: Colors.white),
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-              SizedBox(
-                //여백 주기
-                height: 16,
-              ),
-              ClipRRect(
-                //모서리를 둥글게 해주는 위젯
-                borderRadius: BorderRadius.circular(30),
-                child: RaisedButton(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 15.0, horizontal: 50), //버튼에 여백 주기
-                  color: Colors.orange,
-                  child: Icon(Icons.refresh, color: Colors.white),
-                  onPressed: () {},
-                ),
-              )
-            ],
-          ),
-        ),
       ),
     );
+  }
+
+  Color getColor(AirResult result) {
+    if (result.data.current.pollution.aqius <= 50) {
+      return Colors.greenAccent;
+    } else if (result.data.current.pollution.aqius <= 100) {
+      return Colors.yellow;
+    } else if (result.data.current.pollution.aqius <= 150) {
+      return Colors.orange;
+    } else {
+      return Colors.red;
+    }
+  }
+
+  String getString(AirResult result) {
+    if (result.data.current.pollution.aqius <= 50) {
+      return '좋음';
+    } else if (result.data.current.pollution.aqius <= 100) {
+      return '보통';
+    } else if (result.data.current.pollution.aqius <= 150) {
+      return '나쁨';
+    } else {
+      return '최악';
+    }
   }
 }
